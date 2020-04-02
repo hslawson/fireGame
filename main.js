3b9f9412e523aee601703dbc1ae33c15b6c79c00
Helen Lawson <42982632+hslawson@users.noreply.github.com>
2020-04-02 16:55:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+document.getElementById("fireButton").style.display = "none";
+
+var wood = 0;
+var flint = false;
+
+function gather(number){
+	wood = wood + number;
+	flint = true;
+	document.getElementById("wood").innerHTML = wood;
+	document.getElementById("flint").innerHTML = flint;
+	if(wood>= 5 && flint== true){
+		document.getElementById("fireButton").style.display = "block";
+	};
+		
+};
+
+var fire = false;
+var fireSize = 1;
+
+function fire(){
+	fire = false;
+
+};
+
+function buildFire(){
+	var fireCost = Math.floor(10 * Math.pow(1.1, fireSize));
+	if(wood >= fireCost){
+		fire = true;
+		wood = wood - fireCost;
+		fireSize = fireSize + 1;
+		document.getElementById('fire').innerHTML = fire;
+		document.getElementById('wood').innerHTML = wood;
+
+	};
+	var nextCost = Math.floor(10 * Math.pow(1.1, fireSize));
+	document.getElementById('fireCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+	// gatherWood(golems);
+	setTimeout(fire(), fireSize)
+
+}, 1000);
